378c11df96738051338fd6a76d7f7a78b622a494
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-20 16:56:00 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -99,7 +99,7 @@ int main(int argc, char **argv) {
            (write_txt_file ? output_file_txt : "--nicht gewaehlt--"));
 
 //prep x und y
-    unsigned long long curve_length = (unsigned long long)1 << (2 * degree);
+    unsigned long long curve_length = (unsigned long long)(1) << (2 * degree);
     x = malloc(sizeof(coord_t)*curve_length);
     y = malloc(sizeof(coord_t)*curve_length);
     

f8ed2399217b7e6fb41c839a028e514302d69f19
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-20 17:01:13 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -168,7 +168,7 @@ void add_segments(unsigned segment_degree, coord_t* x, coord_t* y){
         segment_coord--;
         //left upper segment
         //printf("%lld\n", segment_length);
-        //x[segment_length].val = x[0].val;
+        //x[segment_length + i].val = x[i].val;
         //y[segment_length + i].val = y[i].val + segment_coord;
 
         //right upper segment
@@ -192,7 +192,7 @@ void hilbert(unsigned degree, coord_t* x, coord_t* y) {
     x[0].val = 0; y[0].val = 0; x[1].val = 0; y[1].val = 1; x[2].val = 1; y[2].val = 1; x[3].val = 1; y[3].val = 0;
 
     for(unsigned i = 1; i < degree; ++i){
-        add_segments(i, x, y);
+        //add_segments(i, x, y);
     }
 }
 

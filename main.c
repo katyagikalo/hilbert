1050f8543221181fcf0f3fa4bec6f54c3bdda595
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-20 18:03:22 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -100,7 +100,7 @@ int main(int argc, char **argv) {
 
 //prep x und y
     unsigned long long curve_length = (unsigned long long)1 << (2 * degree);
-    printf("%d\n", curve_length);
+    printf("%lld\n", curve_length);
     x = malloc(sizeof(coord_t)*curve_length);
     if(x == NULL){
         printf("X NULL\n");

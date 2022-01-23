2b0a48c394beef7425ceabc61e7b900c14b215ba
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-23 14:06:38 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -252,11 +252,11 @@ void hilbert_V1(unsigned degree, coord_t* x, coord_t* y) {
 
 void add_segments_simd(unsigned segment_degree, coord_t* x, coord_t* y){
     unsigned long long segment_length = (unsigned long long)1 << (2 * (segment_degree));
-    unsigned /*segment_coord = (1 << segment_degree), */loop_length = segment_length / 4;
+    unsigned segment_coord = (1 << segment_degree);
     
     __m128i arr_x = _mm_loadu_si128((__m128i const*)(x));
     __m128i arr_y = _mm_loadu_si128((__m128i const*)(y));
-   for(unsigned long long i = 0; i < loop_length; i+=4) {
+   for(unsigned long long i = 0; i < segment_length; i+=4) {
        
        _mm_storeu_si128((__m128i*)(x + i), arr_x);
        _mm_storeu_si128((__m128i*)(y + i), arr_y);

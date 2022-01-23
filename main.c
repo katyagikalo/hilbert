4b1a71abc05ae55239858c1dd0fc2b75a99ea867
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-23 12:41:40 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -276,12 +276,15 @@ void add_segments_simd(unsigned segment_degree, coord_t* x, coord_t* y){
         _mm_storeu_si128((__m128i*)(y + i), arr_x);
 
         //right lower segment
+        //x[3*segment_length + i].val = 2*segment_coord - 1 - x[i].val;
+        //y[3*segment_length + i].val = segment_coord - 1 - y[i].val;
+        
         x_offset = _mm_add_epi32(sc, sc);
         __m128i neg_one = _mm_set1_epi32(-1);
         x_offset = _mm_add_epi32(x_offset, neg_one);
         x_offset = _mm_sub_epi32(x_offset, arr_x);
         _mm_storeu_si128((__m128i*)(x + 3*segment_length + i), x_offset);
-        y[3*segment_length + i].val = segment_coord - 1 - y[i].val;
+        //y[3*segment_length + i].val = segment_coord - 1 - y[i].val;
         y_offset = _mm_add_epi32(sc, neg_one);
         y_offset = _mm_sub_epi32(y_offset, arr_y);
         _mm_storeu_si128((__m128i*)(y + 3*segment_length + i), y_offset);

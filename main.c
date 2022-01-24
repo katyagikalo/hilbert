51b605944a45809c9bcd90b42df9f8c508acf04e
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-24 17:51:20 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
     int option;
     int option_index = 0;
     static struct option long_options[] = {
-        {"help", no_argument, 0, 'h'}
+        {"help", no_argument, 0, 'h'},
         {0,0,0,0}
     };
 

089df00291296181eaf6db40075ec5613e1c799a
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-27 18:52:08 +0100

    Add files via upload

--- main.h
+++ main.h
@@ -12,7 +12,7 @@ typedef struct {
     bool messure_time, write_svg_file, write_txt_file, print_console;
     char *output_file_svg, *output_file_txt;
     struct timespec *start, *end;
-    coord_t *x, y;
+    coord_t *x, *y;
 }parameter;
 
 double v_assembly(unsigned degree, coord_t x, coord_t* y);

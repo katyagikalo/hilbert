a1699b964978aececf01086cc13e5241c1f06caf
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-29 12:14:09 +0100

    Add files via upload

--- hilbert.h
+++ hilbert.h
@@ -1,6 +1,15 @@
 #ifndef HILBERT_H
 #define HILBERT_H
 
+typedef struct{
+    coord_t *x;
+    coord_t *y;
+    unsigned long long segment_length;
+    unsigned segment_coord;
+    unsigned start;
+    unsigned end;
+}pthread_args;
+
 void hilbert_V0(unsigned degree, coord_t* x, coord_t* y);
 void hilbert_V1(unsigned degree, coord_t* x, coord_t* y);
 void hilbert_V2(unsigned degree, coord_t* x, coord_t* y);

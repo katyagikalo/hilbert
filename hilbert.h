cb9bf3ac9a7f36e50144dc11c72c2ba2ce08d953
katya <61763880+katyagikalo@users.noreply.github.com>
2022-01-31 14:15:25 +0100

    Add files via upload

--- hilbert.h
+++ hilbert.h
@@ -1,15 +1,6 @@
 #ifndef HILBERT_H
 #define HILBERT_H
 
-typedef struct{
-    coord_t *x;
-    coord_t *y;
-    unsigned long long segment_length;
-    unsigned segment_coord;
-    unsigned start;
-    unsigned end;
-}pthread_args;
-
 void hilbert_V0(unsigned degree, coord_t* x, coord_t* y);
 void hilbert_V1(unsigned degree, coord_t* x, coord_t* y);
 void hilbert_V2(unsigned degree, coord_t* x, coord_t* y);
@@ -20,7 +11,8 @@ void add_segments(unsigned degree, coord_t* x, coord_t* y);
 void add_segments_simd(unsigned segment_degree, coord_t* x, coord_t* y);
 void * add_segments_simd_multithreaded(void * args);
 void * add_segments_multithreaded(void * args);
-void * v_assembly(pthread_args * args);
+
+void v_assembly(unsigned degree, coord_t* x, coord_t* y);
 
 #endif
 

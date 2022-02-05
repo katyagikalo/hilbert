2f0949f876be9b6f3205401495b8f4e4e4658d2b
katya <61763880+katyagikalo@users.noreply.github.com>
2022-02-05 22:45:39 +0100

    Add files via upload

--- print.h
+++ print.h
@@ -4,7 +4,7 @@
 void print_parameter(parameter parameter_args);
 void print_curve(unsigned degree, coord_t* x, coord_t* y);
 double print_time(struct timespec start, struct timespec end);
-void print_result(unsigned degree, double result[][7]);
+void print_result(unsigned degree, double result[][6]);
 void create_folder(char path[]);
 void write_svg(char *path, char *output_file_svg, int degree, coord_t* x, coord_t* y);
 void write_txt(char *path, char* output_file_txt, int degree, coord_t* x, coord_t* y);
